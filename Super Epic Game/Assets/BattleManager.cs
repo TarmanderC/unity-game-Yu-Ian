dcddbb40c0a6f2b2c0c805a47ed28b8cdecf7559
TarmanderC <88121443+TarmanderC@users.noreply.github.com>
2024-12-15 15:09:38 -0500

    12/15/2024 v2

--- Super Epic Game/Assets/BattleManager.cs
+++ Super Epic Game/Assets/BattleManager.cs
@@ -3,22 +3,29 @@ using UnityEngine;
 public class BattleManager : MonoBehaviour
 {
     private bool isBattleActive = false;
+    private Transform playerTransform;
     public Transform cameraTransform;
+
+    void Awake() {
+        playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        cameraTransform.position = new Vector3(0,0,-10);
     }
 
     public void StartBattle(GameObject player, GameObject enemy) {
         if (!isBattleActive) {
             isBattleActive = true;
+            cameraTransform.position = new Vector3(-25,-25,-10);
             Debug.Log($"Battle started with {player.name} and {enemy.name}");
         }
     }
 
     public void EndBattle() {
         isBattleActive = false;
+        cameraTransform.position = playerTransform.position;
         Debug.Log("Battle ended!");
     }
 
