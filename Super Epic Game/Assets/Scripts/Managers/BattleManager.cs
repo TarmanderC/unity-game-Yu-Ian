24aa106de55593bf760fe08bf50a2924b167c36c
TarmanderC <88121443+TarmanderC@users.noreply.github.com>
2024-12-15 18:47:41 -0500

    Added some more

--- Super Epic Game/Assets/Scripts/Managers/BattleManager.cs
+++ Super Epic Game/Assets/Scripts/Managers/BattleManager.cs
@@ -1,16 +1,22 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BattleManager : MonoBehaviour
 {
-    public List<GameObject> players;
-    public List<GameObject> enemies;
+    public GameObject[] players;
+    public GameObject[] enemies;
     private bool isBattleActive = false;
     private Transform playerTransform;
     public Transform cameraTransform;
+    public Transform playerStage;
+    public Transform enemyStage;
+
+    private KnightMovement playerMovement;
 
     void Awake() {
         playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+        playerMovement = GameObject.Find("Player").GetComponent<KnightMovement>();
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,26 +24,47 @@ public class BattleManager : MonoBehaviour
         cameraTransform.position = new Vector3(0,0,-10);
     }
 
-    public void StartBattle(List<GameObject> players, List<GameObject> enemies) {
+    public void StartBattle(GameObject[] players, GameObject[] enemies) {
+        
         if (!isBattleActive) {
+            playerMovement.canMove = false;
+
             isBattleActive = true;
             cameraTransform.position = new Vector3(-25,-25,-10);
             this.players = players;
             this.enemies = enemies;
+
+            setUpPlayers();
+        }
+    }
+
+    private void setUpPlayers() {
+        for (int i = 0; i < players.Length; i++) {
+            if (players[i] != null) {
+                switch (i)
+                {
+                    case 0:
+                        Instantiate(players[i], playerStage);
+                        break;
+                }
+            }
         }
     }
 
     public void EndBattle() {
         isBattleActive = false;
+        playerMovement.canMove = true;
         cameraTransform.position = playerTransform.position;
-        players.Clear();
-        enemies.Clear();
+        
+        Array.Clear(players, 0, players.Length);
+        Array.Clear(enemies, 0, players.Length);
+        
         Debug.Log("Battle ended!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
